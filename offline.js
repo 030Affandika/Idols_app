cbe82bd17b92678a80b73ba75fecf8e7d51469c6
030Affandika <100705931+030Affandika@users.noreply.github.com>
2024-01-11 13:26:45 +0700

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,28 @@
+﻿{
+	"version": 1704952745,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/mainmenuidols-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/manajemendiri-sheet0.png",
+		"images/apaitu-sheet0.png",
+		"images/bersabahat-sheet0.png",
+		"images/gejala-sheet0.png",
+		"images/game-sheet0.png",
+		"images/komunitas-sheet0.png",
+		"images/peran-sheet0.png",
+		"images/uluran-sheet0.png",
+		"images/close-sheet0.png",
+		"images/back-sheet0.png",
+		"images/cobabuttton-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
